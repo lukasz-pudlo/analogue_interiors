7d26ea0a6e552c713b180a234641f19c510c5567
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-06-11 00:06:10 +0100

    Return self.name

--- analogue_interiors_blog/projects/models.py
+++ analogue_interiors_blog/projects/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+
 class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     date_added = models.DateTimeField(auto_now=True)
     date_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
