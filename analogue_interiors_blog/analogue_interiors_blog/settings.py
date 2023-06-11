39d65ae4a87b2d5b9f79599d8c33b53ed113c486
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-06-11 12:33:53 +0100

    Set up a local postgresql db with Docker

--- analogue_interiors_blog/analogue_interiors_blog/settings.py
+++ analogue_interiors_blog/analogue_interiors_blog/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'analogue_interiors_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("POSTGRES_DB"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
