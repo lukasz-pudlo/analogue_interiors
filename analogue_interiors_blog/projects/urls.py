10ccf7e3fafa7d97a6500a3197c557b73d5783af
Lukasz Pudlo <l.pudlo1990@gmail.com>
2023-06-11 09:42:58 +0100

    Fix the project detail view

--- analogue_interiors_blog/projects/urls.py
+++ analogue_interiors_blog/projects/urls.py
@@ -4,11 +4,10 @@ from projects.views import ProjectListView, ProjectDetailView, ProjectCreateView
 app_name = 'projects'
 
 urlpatterns = [
-    path('', ProjectListView.as_view(), name='projects'),
-    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
-    path('project/new/', ProjectCreateView.as_view(), name='project_new'),
-    path('project/<int:pk>/edit/', ProjectUpdateView.as_view(), name='project_edit'),
+    path('', ProjectListView.as_view(), name='list'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='detail'),
+    path('project/new/', ProjectCreateView.as_view(), name='new'),
+    path('project/<int:pk>/edit/', ProjectUpdateView.as_view(), name='edit'),
     path('project/<int:pk>/delete/',
-         ProjectDeleteView.as_view(), name='project_delete')
-
+         ProjectDeleteView.as_view(), name='delete')
 ]
--- analogue_interiors_blog/projects/views.py
+++ analogue_interiors_blog/projects/views.py
@@ -5,26 +5,26 @@ from projects.models import Project
 
 class ProjectListView(ListView):
     model = Project
-    template_name = 'analogue_interiors_blog/templates/projects/project_list.html'
+    template_name = 'projects/project_list.html'
     context_object_name = 'projects'
 
 
 class ProjectDetailView(DetailView):
     model = Project
-    template_name = 'analogue_interiors_blog/templates/projects/project_detail.html'
+    template_name = 'projects/project_detail.html'
 
 
 class ProjectCreateView(CreateView):
     model = Project
-    template_name = 'analogue_interiors_blog/templates/projects/project_form.html'
+    template_name = 'projects/project_form.html'
 
 
 class ProjectUpdateView(UpdateView):
     model = Project
-    template_name = 'analogue_interiors_blog/templates/projects/project_form.html'
+    template_name = 'projects/project_form.html'
 
 
 class ProjectDeleteView(DeleteView):
     model = Project
-    template_name = 'analogue_interiors_blog/templates/projects/project_confirm_delete.html'
+    template_name = 'projects/project_confirm_delete.html'
     success_url = reverse_lazy('projects')
